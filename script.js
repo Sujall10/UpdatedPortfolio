7b361f75f60a2b77b39d816beb83c5973c89656d
Sujall10 <94795609+Sujall10@users.noreply.github.com>
2024-06-01 00:02:37 +0530

    commit completed

--- script.js
+++ script.js
@@ -3,28 +3,7 @@ document.querySelector('.scroll-up').addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
-// document.getElementById('contact').addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     // Get form values
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const message = document.getElementById('message').value;
-
-//     // Validate form values (basic validation)
-//     if (name === '' || email === '' || message === '') {
-//         displayStatusMessage('All fields are required.', 'error');
-//         return;
-//     }
-
-//     // Simulate form submission (you can replace this with an actual form submission)
-//     setTimeout(() => {
-//         displayStatusMessage('Message sent successfully!', 'success');
-//     }, 5000);
-// });
-
-// function displayStatusMessage(message, type) {
-//     const statusMessage = document.getElementById('statusMessage');
-//     statusMessage.textContent = message;
-//     statusMessage.className = type;
-// }
+AOS.init({
+    duration: 1000, // Duration of the animation in milliseconds
+    // Whether animation should happen only once - while scrolling down
+});
